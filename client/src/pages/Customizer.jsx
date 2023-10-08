6c37198e8ac03f12f66a572f5fa190bf4efb557d
Ngô Việt Anh <nva@netcompany.com>
2023-10-08 15:33:56 +0700

    update api

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -37,7 +37,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle/", {
+      const response = await fetch("https://threed-with-three-js.onrender.com/api/v1/dalle/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt: prompt }),
